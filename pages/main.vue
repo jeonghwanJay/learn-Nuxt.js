1bff303c135d65228ca7db83cc48120a42939984
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-05 15:47:42 +0900

    add : main.vue, backend folder

---
+++ pages/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Main Page12345</h1>
+    <p>{{products}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    async asyncData() {
+        const response = await axios.get('http://localhost:3000/products')
+        console.log(response);
+        this.products = response.data
+    },
+    data() {
+        return {
+            products: [],
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
