4b23470d28f54dd2b08d412cbbc0e44811d65377
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-05 18:39:24 +0900

    add : css folder, reset.css edit : nuxt.config.js, _id.vue

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <p>name : {{product.name}}</p>
-    <img :src="product.imageUrl" :alt="product.name">
-    <p>price : {{product.price}}</p>
+ <div>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+        <!-- <button type="button" @click="addToCart">Add to Cart</button> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +29,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 </style>
