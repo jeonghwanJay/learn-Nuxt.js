e9aab1efc1b2c0236320dd5998b7193d342844c5
jeonghwanJay <nawhgnoejij@naver.com>
2022-11-07 15:09:57 +0900

    add : index.js(in store folder) edit : cart.vue, _id.vue

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    장바구니 페이지입니다.
+    <h1>장바구니 페이지</h1>
+    <div>
+        <ul>
+            <li v-for="cartItem in $store.state.cartItems" :key="cartItem.id">
+                <img :src="cartItem.imageUrl" :alt="cartItem.name">
+                <p>{{cartItem.name}}</p>
+            </li>
+        </ul>
+        <button>구매하기</button>
+    </div>
   </div>
 </template>
 
